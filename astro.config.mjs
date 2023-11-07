4e63663b0e9a96c9625ca99554e1a1dfceebeb13
NadeemShareef <nadeemshareef934@gmail.com>
2023-11-07 21:20:28 +0530

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,34 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Learn Firebase",
+      social: {
+        github: "https://github.com/shareef99",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+          collapsed: false,
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+          collapsed: true,
+        },
+      ],
+    }),
+  ],
+  server: {
+    open: true,
+    port: 3000,
+    host: true,
+  },
 });
