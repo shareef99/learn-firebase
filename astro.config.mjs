379c488739a42b958d0721373b483a4cf1bdd0d1
NadeemShareef <nadeemshareef934@gmail.com>
2023-11-07 21:40:47 +0530

    Add vercel link for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://learn-firebase-shareef.vercel.app/",
   integrations: [
     starlight({
       title: "Learn Firebase",
